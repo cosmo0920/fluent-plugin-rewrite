435d3424d1adc4fdcaf9668d94de1bf1d1ad1d0c
Hiroshi Hatake <hatake@clear-code.com>
2015-10-26 10:20:37 +0900

    Extract common code (WIP)

---
+++ lib/fluent/plugin/rewrite_rule.rb
@@ -0,0 +1,52 @@
+module Fluent
+  class RewriteRule
+    attr_reader :rules
+
+    def initialize(plugin, conf)
+      @rules = conf.elements.select {|element| element.name == 'rule' }.map do |element|
+        rule = {}
+        element.keys.each do |key|
+          # read and throw away to supress unread configuration warning
+          rule[key] = element[key]
+        end
+        rule["regex"] = Regexp.new(element["pattern"]) if element.has_key?("pattern")
+        rule
+      end
+    end
+
+    def rewrite(record)
+      @rules.each do |rule|
+        record, last = apply_rule(rule, record)
+
+        break  if last
+        return if !record
+      end
+
+      [record]
+    end
+
+    def apply_rule(rule, record)
+      key        = rule["key"]
+      pattern    = rule["pattern"]
+      last       = nil
+
+      return [record] if !key || !record.has_key?(key)
+      return [record] unless pattern
+
+      if matched = record[key].match(rule["regex"])
+        return if rule["ignore"]
+
+        if rule["replace"]
+          replace = rule["replace"]
+          record[key] = record[key].gsub(rule["regex"], replace)
+        end
+
+        if rule["last"]
+          last = true
+        end
+      end
+
+      [record, last]
+    end
+  end
+end
